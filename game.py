3ad827a8c2506d05f853df59001ded3eab371209
marcus-elia <mselia@uvm.edu>
2020-08-30 00:06:53 -0400

    Added jumping

--- game.py
+++ game.py
@@ -21,10 +21,21 @@ class Game(ShowBase):
         
         self.environment = loader.loadModel("Models/Environment/ground.bam")
         self.environment.reparentTo(render)
+        
+        self.initializeKeyMap()
+        self.initializePlayer()
 
         self.camera.setPos(0, 0, 5)
         self.camera.setP(-30)
-        
+                
+        self.updateTask = taskMgr.add(self.update, "update")
+    
+    # ============================================
+    #
+    #      Initialization Helper Functions
+    #
+    # ============================================
+    def initializeKeyMap(self):
         self.keyMap = {
             "up" : False,
             "down" : False,
@@ -41,29 +52,54 @@ class Game(ShowBase):
         self.accept("a-up", self.updateKeyMap, ["left", False])
         self.accept("d", self.updateKeyMap, ["right", True])
         self.accept("d-up", self.updateKeyMap, ["right", False])
-        self.accept("mouse1", self.updateKeyMap, ["jump", True])
-        self.accept("mouse1-up", self.updateKeyMap, ["jump", False])
-        
-        self.updateTask = taskMgr.add(self.update, "update")
+        self.accept("space", self.updateKeyMap, ["jump", True])
+        self.accept("space-up", self.updateKeyMap, ["jump", False])
     
+    def initializePlayer(self):
+        self.playerHeight = 10
+        self.gravity = 10.0
+        self.jumpStrength = 5.0
+        self.playerXYSpeed = 5.0
+        self.playerZSpeed = 0.0
+        self.playerIsGrounded = True
+
+    # ============================================
+    #
+    #          Update Helper Functions
+    #
+    # ============================================
     def updateKeyMap(self, controlName, controlState):
         self.keyMap[controlName] = controlState
     
     def update(self, task):
         dt = globalClock.getDt()
+        s = self.playerXYSpeed
 
         if self.keyMap["up"]:
-            self.camera.setPos(self.camera.getPos() + Vec3(0, 5.0*dt, 0))
+            self.camera.setPos(self.camera.getPos() + Vec3(0, s*dt, 0))
         if self.keyMap["down"]:
-            self.camera.setPos(self.camera.getPos() + Vec3(0, -5.0*dt, 0))
+            self.camera.setPos(self.camera.getPos() + Vec3(0, -s*dt, 0))
         if self.keyMap["left"]:
-            self.camera.setPos(self.camera.getPos() + Vec3(-5.0*dt, 0, 0))
+            self.camera.setPos(self.camera.getPos() + Vec3(-s*dt, 0, 0))
         if self.keyMap["right"]:
-            self.camera.setPos(self.camera.getPos() + Vec3(5.0*dt, 0, 0))
+            self.camera.setPos(self.camera.getPos() + Vec3(s*dt, 0, 0))
         if self.keyMap["jump"]:
-            print ("jump!")
+            self.tryToJump()
+        
+        if not self.playerIsGrounded:
+            self.camera.setPos(self.camera.getPos() + Vec3(0, 0, self.playerZSpeed*dt))
+            self.playerZSpeed -= self.gravity*dt
+            if self.camera.getPos().z < self.playerHeight/2:
+                self.playerIsGrounded = True
+                self.playerZSpeed = 0.0
+                self.camera.setPos(self.camera.getPos().x, self.camera.getPos().y, self.playerHeight/2)
 
         return task.cont
+    
+    def tryToJump(self):
+        if self.playerIsGrounded:
+            self.playerZSpeed = self.jumpStrength
+            self.playerIsGrounded = False
 
 game = Game()
 game.run()
